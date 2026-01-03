57d0ca704dd88943f11b3a10f1dc0171bd08ec92
Annett Hossain <annetthossain@outlook.com>
2026-01-03 23:00:17 +0600

    switch to zig

--- src/com.hxx
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <vector>
-#include <string>
-
-void trash_files(const std::vector<std::string>& files);
--- src/trash.cxx
+++
@@ -1,9 +0,0 @@
-#include <com.hxx>
-#include <vector>
-#include <string>
-
-void trash_files(const std::vector<std::string>& files) {
-  for (auto& file : files) {
-    
-  }
-}
