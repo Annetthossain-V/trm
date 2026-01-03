dc4d807202acddf0bea831db07f1ce799552e521
Annett Hossain <annetthossain@outlook.com>
2026-01-04 01:19:06 +0600

    add command line handler

---
+++ src/err.zig
@@ -0,0 +1,3 @@
+
+
+pub const err = error { CmdlineFailed };
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,19 @@
 const std = @import("std");
+const cmdline = @import("cmdline.zig");
 
 pub fn main() !u8 {
-    return 0;
+  var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+  defer _ = gpa.deinit();
+
+  var cmd = cmdline.parseCmdline(gpa.allocator()) catch  {
+    std.debug.print("Unable to parse command line args!\n", .{});
+    return 1; 
+  };
+  defer cmd.deinit();
+
+  if (cmdline.checkCmd(&cmd)) return 0;
+  
+  
+
+  return 0;
 }
