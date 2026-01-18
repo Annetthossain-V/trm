73fcc3775cfd03bb4b09fd6df914e98a7398af9e
Annett Hossain <annetthossain@gmail.com>
2026-01-18 21:39:36 +0600

    implement trash_cat_init function

--- src/trash_init.cc
+++ src/trash_init.cc
@@ -1,5 +1,8 @@
 #include "trash_init.h"
+#include "trmdef.h"
+#include <cstdio>
 #include <cstdlib>
+#include <cstring>
 #include <filesystem>
 #include <print>
 #include <string>
@@ -37,7 +40,41 @@ void init_trash_cat(const bool create) {
   std::string TrashCatPath(HomeDir);
   TrashCatPath += "/.trm/trash.cat";
 
-  throw int{2};
+  fs::path path(TrashCatPath);
+  if (!fs::exists(path) && !create) {
+    std::println("Trash Catalog File `{}` Not Found!", TrashCatPath);
+    throw std::runtime_error("Unable to find catalog file");
+  }
+  if (fs::is_directory(path)) {
+    std::println("Trash Catalog Path `{}` Is A Directory", TrashCatPath);
+    throw std::runtime_error("Expected File, Found Directory");
+  }
+
+  if (!fs::exists(path) && create) {
+    _file TrashCat = std::fopen(TrashCatPath.c_str(), "wb");
+    if (TrashCat.ptr == nullptr) {
+      std::println("Failed to create trash catalog `{}`", TrashCatPath);
+      throw std::runtime_error("Failed to create file");
+    }
+
+    std::uint64_t sig[] = TRASH_CATALOG_SIGNATURE;
+    fwrite_checked(&sig, sizeof(std::uint64_t), 4, TrashCat.ptr);
+  }
+
+  _file TrashCat = std::fopen(TrashCatPath.c_str(), "rb");
+  if (TrashCat.ptr == nullptr) {
+    std::println("Failed to open trash catalog file `{}`", TrashCatPath);
+    throw std::runtime_error("File not open");
+  }
+
+  std::uint64_t sig[4];
+  std::uint64_t check_sig[] = TRASH_CATALOG_SIGNATURE;
+
+  fread_checked(&sig, sizeof(std::uint64_t), 4, TrashCat.ptr);
+  if (std::memcmp(&sig, &check_sig, sizeof(std::uint64_t) * 4) != 0) {
+    std::println("Trash catalog file signature does not match!");
+    throw std::runtime_error("Corrupted Catalog file");
+  }
 
   return;
 }
--- src/trmdef.h
+++ src/trmdef.h
@@ -1,15 +1,37 @@
 #ifndef _TRMDEF_H
 #define _TRMDEF_H
 #include <cstdint>
+#include <cstdio>
+#include <stdexcept>
 
-#define TRASH_CATALOG_SIGNATURE1                                               \
+#define TRASH_CATALOG_SIGNATURE                                                \
   {0xFFF20AE922, 0xFF87721EFAC, 0x21BCDAE9932C, 0xFFAACB229392}
 
-struct TrashCatalog {
-  std::uint64_t _sig1[4];
-  char *files_list;
-  std::uint32_t files_list_len;
-} __attribute__((packed));
-typedef struct TrashCatalog TrashCatalog;
+static inline void fwrite_checked(void *ptr, size_t size, size_t n,
+                                  std::FILE *file) {
+  if (std::fwrite(ptr, size, n, file) != n)
+    throw std::runtime_error("failed to write to file");
+}
+
+static inline void fread_checked(void *ptr, size_t size, size_t n,
+                                 std::FILE *file) {
+  if (std::fread(ptr, size, n, file) != n)
+    throw std::runtime_error("failed to read file");
+}
+
+struct _file {
+  std::FILE *ptr;
+
+  _file() { ptr = nullptr; }
+  _file(std::FILE *fp) { ptr = fp; }
+  ~_file() {
+    if (ptr != nullptr)
+      std::fclose(ptr);
+  }
+  _file &operator=(std::FILE *fp) {
+    ptr = fp;
+    return *this;
+  }
+};
 
 #endif // _TRMDEF_H
